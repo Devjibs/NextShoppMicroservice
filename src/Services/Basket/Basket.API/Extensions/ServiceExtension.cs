43126f3d120300a71a82ebdba96b4befcd7e63be
Habeeb Ajide <Ajidejibola@yahoo.com>
2022-01-18 23:14:00 +0100

    Created BasketAPI Microservice

---
+++ src/Services/Basket/Basket.API/Extensions/ServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using Basket.API.Repositories.Interfaces;
+using Basket.API.Repositories.Services;
+
+namespace Basket.API.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void RegisterDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+            services.AddScoped<IBasketRepository, BasketRepository>();
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Mapper/BasketProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+
+namespace Basket.API.Mapper
+{
+	public class BasketProfile : Profile
+	{
+		//public BasketProfile()
+		//{
+		//	CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+		//}
+	}
+}
