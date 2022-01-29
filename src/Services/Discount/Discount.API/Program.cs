a8a137cc171442a75537d88667dcf58eb43d3445
Habeeb Ajide <hajide@coure-tech.com>
2022-01-29 19:56:33 +0100

    Created Discount.Grpc Service

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,13 +1,19 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories.Interfaces;
+using Discount.API.Repositories.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
